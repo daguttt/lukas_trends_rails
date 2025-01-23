791d22e33388ac93a470d5069324f78bfdbeaf86
Juan Camilo Rojas <163916498+jucrojasba@users.noreply.github.com>
2025-01-23 10:48:39 -0500

    Task 005/000/home page (#8)
    * feat: modify database connection configuration, add home page view, controller, and route; implement basic layout in view and apply global styles in application stylesheet
    
    * feat: add 'videos' folder in assets, upload animated logo video, and upload logo to 'images' folder
    
    * feat: add the view of chatgpt
    
    * feat: Complete navbar feature in layout
    
    * feat: add link tags to navbar feature
    
    * feat: complet chart
    
    * style: format files with rubocop
    
    * Fix: update db connection
    
    ---------
    
    Co-authored-by: leison <leison3131@hotmail.com>

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,39 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import "@hotwired/turbo-rails"
-import "controllers"
+import "@hotwired/turbo-rails";
+import "controllers";
+console.log("Hermosis");
+document.addEventListener("DOMContentLoaded", function () {
+  const chartContainer = document.getElementById("echart-container");
+  if (!chartContainer) {
+    console.error("El contenedor del gráfico no se encuentra.");
+    return;
+  }
+
+  const chart = echarts.init(chartContainer);
+
+  // Configuración más simple para depuración
+  const option = {
+    title: {
+      text: "Simple Line Chart",
+    },
+    tooltip: {
+      trigger: "axis",
+    },
+    xAxis: {
+      type: "category",
+      data: ["2021", "2022", "2023", "2024", "2025"],
+    },
+    yAxis: {
+      type: "value",
+    },
+    series: [
+      {
+        name: "Line",
+        type: "line",
+        data: [120, 132, 101, 134, 90],
+      },
+    ],
+  };
+
+  chart.setOption(option);
+});
